29b4a34d86399aefd793d26879ebc2748b83300f
Mayur Ramani <ramanimayur836@gmail.com>
2025-08-16 12:36:07 +0530

    Integrate MongoDb

--- Backend/server.js
+++ Backend/server.js
@@ -1,8 +1,10 @@
 const express = require("express");
-const dotenv = require("dotenv")
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
 
 
 dotenv.config();
+connectDB();
 const app = express();
 
 app.get("/", (req,res) => {
